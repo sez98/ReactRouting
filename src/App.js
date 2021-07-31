ef3d82519a0acbe1090d8a37f554ca1d084b0d2c
sez98 <sezal17101@iiitd.ac.in>
2021-07-31 10:13:28 +0530

    initialization of routing section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Breeds from "./components/Breeds";
 
 const App = () => {
   return (
     <>
-      <div className="App">
-        Hello
-      </div>
+      <BrowserRouter>
+        <header>
+          HEADING WEBSITE
+          <hr />
+          <Link to={{
+            pathname:'/'
+              }}>Home</Link> -
+          <Link to={{
+            pathname:'/about'
+              }}>About</Link> - 
+          <Link to={{
+            pathname:'/breeds'
+              }}>Breeds</Link> - 
+        </header>
+        <hr />
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/breeds" component={Breeds}/>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Breeds.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Breeds = (props) => {
+         console.log(props);
+  return (
+    <>
+      <div>
+         Breeds
+      </div>
+    </>
+  );
+}
+
+export default Breeds;
